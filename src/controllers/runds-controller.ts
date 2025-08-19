6fbe6d98ad1872bfc43a3b69a24648ee9c17a575
Kevin Solar <kevinleitesolar@hotmail.com>
2025-08-19 20:58:39 -0300

    feat: create the validation of data and adding the solicitation into the db

--- src/controllers/runds-controller.ts
+++ src/controllers/runds-controller.ts
@@ -1,9 +1,40 @@
+import { prisma } from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
 import { Request, Response } from "express"
+import z from "zod"
+
+const CategoriesEnum = z.enum(["food", "others", "transport", "accommodation"])
 
 class RefundsController {
-  async create(request: Request, response: Response) {
-    return response.json({ message: "OK" })
-  }
+	async create(request: Request, response: Response) {
+		const bodySchema = z.object({
+			name: z
+				.string()
+				.trim()
+				.min(1, { message: "Informe o nome da solicitação" }),
+			category: CategoriesEnum,
+			amount: z.number().positive({ message: "O valor precisa ser positivo" }),
+			filename: z.string().min(20),
+		})
+
+		const { name, category, amount, filename } = bodySchema.parse(request.body)
+
+		if (!request.user?.id) {
+			throw new AppError("Unauthorized", 401)
+		}
+
+		const refund = await prisma.refunds.create({
+			data: {
+				name,
+				category,
+				amount,
+				filename,
+				userId: request.user.id,
+			},
+		})
+
+		return response.status(201).json(refund)
+	}
 }
 
 export { RefundsController }
